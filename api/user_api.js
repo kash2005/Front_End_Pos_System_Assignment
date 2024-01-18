4d8d4509010c2563584ecbdea56a8d1eb639db4c
kash2005 <kashmifernando3@gmail.com>
2024-01-18 09:57:59 +0530

    add user api

---
+++ api/user_api.js
@@ -0,0 +1,64 @@
+export class User_api{
+    saveUser(user){
+        const userJson = JSON.stringify(user);
+
+        const sendAjax = (userJson) =>{
+            $.ajax({
+                url:"http://localhost:8080/page/user",
+                type:"POST",
+                data:userJson,
+                contentType:"application/json",
+                success: function (){
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'User Saved Successful',
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+        }
+
+        console.log('Save user call');
+        sendAjax(userJson);
+    }
+
+    getUser(userName,password){
+        if (userName){
+            $.ajax({
+                type:"GET",
+                url:"http://localhost:8080/page/user",
+                data:{
+                    action: 'checkUser',
+                    userName: userName
+                },
+                success: function (responseText){
+                    let user = responseText;
+                    if (typeof responseText === 'string'){
+                        // If it's a string, parse it as JSON
+                        user = JSON.parse(responseText);
+                    }
+
+                    if (userName === user.userName && password === user.password) {
+                        const loadingScreen = $('#login_page');
+                        loadingScreen.hide();
+
+                        const loadingScreen1 = $('#dashboard_page');
+                        loadingScreen1.show();
+
+                        const loadingScreen2 = $('#home_page');
+                        loadingScreen2.show();
+
+                        const loadingScreen3 = $('#customer_form');
+                        loadingScreen3.hide();
+
+
+                    }
+                },
+                error: function (xhr, status, error) {
+                    console.log(error);
+                }
+            });
+        }
+    }
+}
