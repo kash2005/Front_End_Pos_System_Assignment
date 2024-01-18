32e68d8e60f178bec8cd8524167bccd6608b0abe
kash2005 <kashmifernando3@gmail.com>
2024-01-18 11:14:28 +0530

    add customer model

---
+++ model/customer_model.js
@@ -0,0 +1,9 @@
+export class CustomerModel{
+    constructor(customerId,customerName,customerAddress,customerContact,customerEmail) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
+        this.customerContact = customerContact;
+        this.customerEmail = customerEmail;
+    }
+}
